92e3c5c6421941a5033101adebcee49f60b835d8
Riley Mosher <rileymosher325@gmail.com>
2023-04-04 21:57:53 -0400

    Created the main function to get all items inside of the grocery list.

--- src/app/services/grocery.service.ts
+++ src/app/services/grocery.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Grocery } from "../models/grocery";
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroceryService {
 
-  constructor() { }
+  URL: string = "localhost.example"
+
+  constructor(private http: HttpClient) { }
 }
