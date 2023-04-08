3250b1577b34c58c000c560278d0b60b23db2a74
Riley Mosher <rileymosher325@gmail.com>
2023-04-07 22:35:03 -0400

    Added the api to sucessfully call. Still need to merge api with search results.

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Recipe } from 'src/app/models/recipe';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+
+recipeList: Recipe[] = [];
+searchSource: string = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=76f259eae67a4d039792ab892368b232&ingredients=apples,+flour,+sugar&number=1"
+
+constructor(private http: HttpClient) { }
+
+findRecipe(): Observable<Recipe[]> {
+  return this.http.get<Recipe[]>(this.searchSource);
+}
+
+searchRecipe(): void {
+  this.findRecipe().subscribe(response => {
+    console.log(response);
+    this.recipeList = response;
+  })
+}
 
 }
