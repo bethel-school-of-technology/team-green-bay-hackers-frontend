320ca4f003680dc259a1b7cc03a2b3af66de4dff
JaedynW <jaedynwhitfield@gmail.com>
2023-04-25 21:29:07 -0700

    Created a sign in message that welcomes the user by their username

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { SignInComponent } from '../sign-in/sign-in.component';
+import { HttpClient, HttpHeaders, HttpContext } from '@angular/common/http';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 
 @Component({
@@ -7,6 +9,44 @@ import { SignInComponent } from '../sign-in/sign-in.component';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
  //here
+  constructor(private userService: UserService) { }
+
+  displayMessage: boolean = false;
+  user: User = new User();
+
+  welcomeMessage(){
+    
+    const userToken = localStorage.getItem('tokenName');
+    let headers = new HttpHeaders().set("Authorization", 'Bearer '  + userToken);
+
+    // var username = this.httpContext.user.findFirst(claimTypes.main).value;
+
+    if(userToken != null){
+      console.log("Welcome " + userToken);
+      this.displayMessage = true;
+    }
+
+    this.userService.getUserInfo(3).subscribe(response => {
+      //Find a way to get user info
+      //Find a way to get the user ID
+      console.log(response);
+      this.user = response;
+    })
+  }
+
+  // getUserInfo(){
+  //   this.userService.getUserInfo(3).subscribe(response => {
+  //     console.log(response);
+  //     this.user = response;
+  //   })
+  // }
+
+
+
+  ngOnInit(): void{
+    this.welcomeMessage();
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { User } from '../models/user';
 import { Router } from '@angular/router';
 import { tap } from 'rxjs';
@@ -22,5 +22,11 @@ export class UserService {
     .pipe(tap((response: any) => {
       localStorage.setItem('tokenName', response.token);
     }))
+  }
+
+  getUserInfo(id: number){
+    const token = localStorage.getItem('tokenName');
+    let headers = new HttpHeaders().set("Authorization", 'Bearer ' + token);
+    return this.http.get(`${this.URL}/${id}`, {headers})
   }
 }
