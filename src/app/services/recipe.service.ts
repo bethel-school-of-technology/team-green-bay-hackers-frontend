ff5bfe37a83d97b34a51166afc64475bdb0a9447
JaedynW <jaedynwhitfield@gmail.com>
2023-04-30 16:34:01 -0600

    recipe details css completed

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -8,10 +8,10 @@ import { HttpClient } from '@angular/common/http';
 })
 export class RecipeService {
 
-  searchSource: string = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=d0bc8082617f45edb163dbcb85175b6e"
+  searchSource: string = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=312098b3d19041978ab0a8c5d5bc0a75"
   searchIdSource: string = "https://api.spoonacular.com/recipes/"
   similarSource: string = "https://api.spoonacular.com/recipes/"
-  randomSource: string = "https://api.spoonacular.com/recipes/random?apiKey=d0bc8082617f45edb163dbcb85175b6e&number=3"
+  randomSource: string = "https://api.spoonacular.com/recipes/random?apiKey=312098b3d19041978ab0a8c5d5bc0a75"
   searchInputParam: string = "&ingredients=";
 
   constructor(private http: HttpClient) { }
